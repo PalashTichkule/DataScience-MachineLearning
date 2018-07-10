3fbcf5713f31c770a69c24d657da877c19683c28
Palash Tichkule <36161088+PalashTichkule@users.noreply.github.com>
2018-07-10 21:37:28 +0530

    Update LinearRegression.py
    Plotting prediction with matplotlib

--- LinearRegression.py
+++ LinearRegression.py
@@ -10,6 +10,11 @@ import math
 import numpy as np
 from sklearn import preprocessing , cross_validation , svm
 from sklearn.linear_model import LinearRegression
+import datetime
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 quandl.ApiConfig.api_key = 'Enter_your_key_here' # Get you key at  https://www.quandl.com/account/api
 
@@ -31,13 +36,14 @@ print("Forcasting {} trading days ahead".format(forecast_out))
 #>>    Forcasting 49 trading days ahead
 
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
 
 X = np.array(df.drop(['label'],1))
-y = np.array(df['label'])
 X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+df.dropna(inplace=True)
+y = np.array(df['label'])
 
-    
 X_train , X_test , y_train , y_test = cross_validation.train_test_split(X,y,test_size = 0.2)
 
 #clf = LinearRegression()
@@ -50,3 +56,27 @@ accuracy = clf.score(X_test,y_test)  # score is for testing
 
 print("Accuracy is",accuracy)
 #>>    Accuracy is 0.970299355595
+
+forecast_set = clf.predict(X_lately)
+
+print(forecast_set, accuracy, forecast_out)
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for z in range(len(df.columns) -1 )]  + [i]
+    #df.loc[] is used to address index of data frame
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
+
